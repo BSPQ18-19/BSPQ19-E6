dc83f69f43d1163908b1d31bc0a2ac724cc33e08
Aratz M. Lasa <aratzml@opendeusto.es>
2019-03-30 22:32:41 +0100

    First JDO working

--- BiddingItServer/src/main/sql/create-biddingdb.sql
+++ BiddingItServer/src/main/sql/create-biddingdb.sql
@@ -1,11 +1,11 @@
 /* DELETE 'biddingdb' database*/
 DROP SCHEMA biddingdb;
 /* DELETE USER 'bidadmin' AT LOCAL SERVER*/
-DROP USER 'bidadmin'@'%';
+DROP USER 'bidadmin'@'localhost';
 
 /* CREATE 'biddingdb' DATABASE */
 CREATE SCHEMA biddingdb;
 /* CREATE THE USER 'bidadmin' AT LOCAL SERVER WITH PASSWORD 'bidpassword' */
-CREATE USER 'bidadmin'@'%' IDENTIFIED BY 'bidpassword';
+CREATE USER 'bidadmin'@'localhost' IDENTIFIED WITH mysql_native_password BY 'bidpassword';
 /* GRANT FULL ACCESS TO THE DATABASE FOR THE USER 'bidadmin' AT LOCAL SERVER*/
-GRANT ALL ON productsDB.* TO 'bidadmin'@'%';
+GRANT ALL ON biddingDB.* TO 'bidadmin'@'localhost';;
