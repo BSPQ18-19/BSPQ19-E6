4228812a06c911c94a5f70c1f9ee181031eff9fc
Aratz M. Lasa <aratzml@opendeusto.es>
2019-03-31 09:44:03 +0200

    Added file for keeping client folder

---
+++ BiddingIt/src/main/java/com/spq/group6/client/App.java
@@ -0,0 +1,13 @@
+package com.spq.group6.server.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
