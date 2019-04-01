48d5536548fe4cb4d72cd9371f3a06dc832ea658
Aratz M. Lasa <aratzml@opendeusto.es>
2019-04-01 16:39:40 +0200

    Added interfaces for DAO

--- BiddingIt/src/main/java/com/spq/group6/server/dao/IAccountDAO.java
+++ BiddingIt/src/main/java/com/spq/group6/server/dao/IAccountDAO.java
@@ -5,4 +5,5 @@ import com.spq.group6.server.data.User;
 public interface IAccountDAO {
     public User createUser(User user);
     public User getUserByUsername(String username);
+    public User updateUser(User user);
 }
