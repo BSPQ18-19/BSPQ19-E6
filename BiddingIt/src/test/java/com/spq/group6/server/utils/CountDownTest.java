582b23a1be9172bf8816b0037a67bdb5371ebc66
Aratz M. Lasa <aratzml@opendeusto.es>
2019-04-24 18:07:27 +0200

    Improved Tests

--- BiddingIt/src/test/java/com/spq/group6/server/utils/CountDownTest.java
+++ BiddingIt/src/test/java/com/spq/group6/server/utils/CountDownTest.java
@@ -1,48 +1,62 @@
 package com.spq.group6.server.utils;
 
-import com.spq.group6.server.dao.AccountDAO;
-import com.spq.group6.server.dao.AuctionDAO;
+import com.spq.group6.server.dao.BiddingDAO;
 import com.spq.group6.server.data.Auction;
 import com.spq.group6.server.data.Product;
 import com.spq.group6.server.data.User;
 import com.spq.group6.server.utils.observer.remote.RemoteObservable;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.Timestamp;
 
+import static org.junit.Assert.assertFalse;
+
 public class CountDownTest {
-    User seller, buyer;
-    Product product;
-    Auction auction;
-    AccountDAO accountDAO = new AccountDAO();
-    AuctionDAO auctionDAO = new AuctionDAO();
+    private User seller, buyer;
+    private Product product;
+    private Auction auction;
+    private BiddingDAO biddingDAO = new BiddingDAO();
+    private int offset;
+    private Thread auctionCountdown;
+
     @Before
     public void setUp(){
         String usernameSeller = "seller", usernameBuyer = "buyer", pass = "test_pass", country = "uk";
         String prodctName = "product", productDescription = "Description";
-        Timestamp limit = new Timestamp(System.currentTimeMillis() + 10000);
-        float initialPrice = 12;
+        offset = 1000;
+        Timestamp limit = new Timestamp(System.currentTimeMillis() + offset);
 
         seller = new User(usernameSeller, pass, country);
         buyer = new User(usernameBuyer, pass, country);
         product = new Product(prodctName, productDescription);
-        auction = new Auction(seller, product, limit, initialPrice, null);
+        auction = new Auction(seller, product, limit, 12, null);
     }
 
     @Test
     public void testCountDown() throws InterruptedException {
-        accountDAO.createUser(seller);
-        accountDAO.createUser(buyer);
-        accountDAO.updateProduct(product);
-        auctionDAO.persistAuction(auction);
-
+        biddingDAO.createUser(seller);
+        biddingDAO.createUser(buyer);
+        biddingDAO.updateProduct(product);
+        biddingDAO.persistAuction(auction);
         AuctionLocks.setLock(auction.getAuctionID()); // create lock for auction
         RemoteObservable observable = new RemoteObservable();
-        Thread auctionCountdown = new Thread(new AuctionCountdown(auction, observable));
+        auctionCountdown = new Thread(new AuctionCountdown(auction, observable));
+        auctionCountdown.start();
 
-        Thread.sleep(2000);
+        Thread.sleep(offset + 1000);
 
-        auctionDAO.isOpen(auction.getAuctionID());
+        AuctionLocks.getLock(auction.getAuctionID()).lock();
+        auction = biddingDAO.getAuctionByID(auction.getAuctionID());
+        assertFalse(auction.isOpen());
+        AuctionLocks.getLock(auction.getAuctionID()).unlock();
+    }
+
+    @After
+    public void tearDown() throws InterruptedException {
+        auctionCountdown.join();
+        biddingDAO.deleteUser(buyer);
+        biddingDAO.deleteUser(seller);
     }
 }
