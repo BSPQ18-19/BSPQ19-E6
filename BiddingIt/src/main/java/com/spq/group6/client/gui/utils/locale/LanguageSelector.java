2d905fa34c04ef87eb90af39bc09e900ff39cdc9
LanderLorenzo <33033661+LanderLorenzo@users.noreply.github.com>
2019-05-19 17:46:34 +0200

    Documentation done (code commented).

--- BiddingIt/src/main/java/com/spq/group6/client/gui/utils/locale/LanguageSelector.java
+++ BiddingIt/src/main/java/com/spq/group6/client/gui/utils/locale/LanguageSelector.java
@@ -3,9 +3,8 @@ package com.spq.group6.client.gui.utils.locale;
 import java.util.Locale;
 
 public class LanguageSelector {
-    /**
-     * Constant corresponding to the English AllowedLocale
-     */
+
+    //English bundle
     public final static LanguageSelector ENGLISH = new LanguageSelector(
             new Locale("en", "US"),
             "en_US",
@@ -13,9 +12,7 @@ public class LanguageSelector {
             "English/US"
     );
 
-    /**
-     * Constant corresponding to the Spanish AllowedLocale
-     */
+    //Spanish bundle
     public final static LanguageSelector SPANISH = new LanguageSelector(
             new Locale("es", "ES"),
             "es_ES",
@@ -23,37 +20,25 @@ public class LanguageSelector {
             "Espanyol/Espanya"
     );
 
-    /**
-     * Constant corresponding to the Basque AllowedLocale
-     */
+    //Basque bundle
     public final static LanguageSelector BASQUE = new LanguageSelector(
             new Locale("eu"),
             "eu",
             "Basque",
             "Euskara"
     );
-    /**
-     * An array with the pre-set allowed locales for the app
-     */
+
     public final static LanguageSelector[] ALLOWED_LOCALES = {ENGLISH, SPANISH, BASQUE};
 
     private Locale locale;
 
-    /**
-     * The language code (e.g.: es_ES for Spanish, Spain)
-     */
     private String code;
 
-    /**
-     * The name of the locale in English
-     */
     private String englishName;
 
-    /**
-     * The localized name of the locale
-     */
     private String localizedName;
 
+    //constructor
     private LanguageSelector(Locale locale, String code, String englishName, String localizedName) {
         this.locale = locale;
         this.code = code;
@@ -61,9 +46,7 @@ public class LanguageSelector {
         this.localizedName = localizedName;
     }
 
-    /**
-     * @return the Locale
-     */
+    //getters
     public Locale getLocale() {
         return locale;
     }
@@ -72,9 +55,6 @@ public class LanguageSelector {
         return code;
     }
 
-    /**
-     * @return the name in English of the locale
-     */
     public String getEnglishName() {
         return englishName;
     }
